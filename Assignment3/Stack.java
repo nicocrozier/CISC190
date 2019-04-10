8d7a51e68d0e00d3b0e842b69490ca614eb0ca98
Nico C <nicholas@paradisbasics.com>
2019-04-09 20:49:56 -0700

    Assignment 3; Expand copy stack

---
+++ Assignment3/Stack.java
@@ -0,0 +1,42 @@
+public class Stack{
+    //Array to hold elements
+    int capacity = 2;
+    int stack[] = new int[capacity];
+    int stackIndex = 0;
+
+    //Push Method; pushes then increments by 1
+    public void push(int dataPushed){
+        if( stackIndex == capacity ){
+            expand();
+            
+        
+        };
+            stack[stackIndex] = dataPushed;
+            stackIndex++;
+        
+    }
+    //Makes a new stack 2x the size of previous Stack
+    public void expand(){
+        int length = stackIndex;
+        int newStack[] = new int[capacity * 2];
+        for( int i = 0; i < capacity; i++){
+            newStack[i] = stack[i];
+        }
+        // Reassign
+        stack = newStack;
+        capacity *= 2;
+        
+    
+    }
+    
+    //Shows whats in the stack
+    public void show(){
+        for (int n : stack){
+            System.out.println(n + "");
+        }
+    }
+    
+}
+
+
+
---
+++ Assignment3/main.java
@@ -0,0 +1,14 @@
+public class main {
+
+    public static void main(String[] args){
+
+        // Stack class, Object
+        Stack nums = new Stack();
+        nums.push(15);
+        nums.push(8);
+        nums.push(55);
+        
+        
+        nums.show();
+    }
+}
